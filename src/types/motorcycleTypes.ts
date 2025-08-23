fe53524284ad8c0972e9aa96815a5b85fc094bc8
Nikola <nikola.dj.87@gmail.com>
2025-08-23 10:28:08 +0200

    hust added more pages

---
+++ src/types/motorcycleTypes.ts
@@ -0,0 +1,38 @@
+export interface IMotorcycle {
+  _id: string;
+  owner: string;
+
+  make: string;
+  model: string;
+  title: string;
+  description: string;
+  motorcycleType:
+    | "sport"
+    | "naked"
+    | "touring"
+    | "cruiser"
+    | "adventure"
+    | "other";
+
+  engineCapacity: number;
+  horsepower: number;
+  transmission: "manual" | "automatic";
+  mileage: number;
+  year: number;
+
+  price: number;
+  isPriceFixed: boolean;
+  availableForPurchase: boolean;
+
+  isNew: boolean;
+  isNewMotorcycle: boolean;
+  keyForKey: string;
+
+  mainImage: string;
+  photos: string[];
+
+  registeredUntil: string;
+  updatedAt: string;
+
+  __v: number;
+}
